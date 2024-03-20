e2a0f00e62b118d06f8a8a779b14b18418a462b5
ccjacobs14 <144470313+ccjacobs14@users.noreply.github.com>
2024-03-19 21:21:20 -0400

    Load balancer

--- main.tf
+++ main.tf
@@ -25,7 +25,6 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -40,12 +39,45 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
   
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
+  subnet_id              = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name   = "blog-alb"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  
+  listeners = {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+  }
+
+  target_groups = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+      }
+  }
+
+  tags = {
+    Environment = "dev"
+    }
 }
 
 module "blog_sg" {
